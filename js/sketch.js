85ed07d0e9d8786cc721c9cdf03bf51343a6e66e
lorossi <mail@lorenzoros.si>
2022-06-09 17:58:58 +0200

    added comments

--- js/sketch.js
+++ js/sketch.js
@@ -1,27 +1,34 @@
 class Sketch extends Engine {
   preload() {
-    this._num = 100;
-    this._horizontal_fraction = 0.8;
+    this._num = 100; // number of elements
+    this._horizontal_fraction = 0.8; // fraction of the line that stays horizontal
   }
 
   setup() {
+    // instantiate the sorter and create a sequence of numbers
     this._sorter = new Sorter(this._num);
-    this._palette = new Palette(this._num);
     this._sorter.generateSequence();
 
+    // instantiate the palette
+    this._palette = new Palette(this._num);
+
+    // set the algorithm to the first one
     this._algorithm_index = 0;
   }
 
   draw() {
+    // pick and algorithm
     const a = this._algorithm_index % this._sorter.algorithms_num;
     console.log(this._sorter.algorithms[a]);
-
+    // sort the sequence in the sorter
     this._sorter.setAlgorithm(a);
     this._sorter.sort();
 
+    // number of steps and position of each number
     const steps = this._sorter.steps;
     const positions = this._sorter.positions;
 
+    // lines scales
     const x_scl = this.width / steps;
     const y_scl = this.height / this._num;
     const length = x_scl * this._horizontal_fraction;
@@ -32,6 +39,7 @@ class Sketch extends Engine {
 
     this.ctx.lineWidth = 2;
 
+    // iterate over each item and draw its corresponding line
     for (let i = 0; i < this._num; i++) {
       let started = false;
       this.ctx.strokeStyle = this._palette.getColour(i).rgb;
@@ -77,14 +85,14 @@ class Sketch extends Engine {
         this.loop();
         break;
       case 97:
-        // A
+        // A, previous algorithm
         this._algorithm_index--;
         if (this._algorithm_index < 0)
           this._algorithm_index += this._sorter.algorithms_num;
         this.loop();
         break;
       case 100:
-        // D
+        // D, next algorithm
         this._algorithm_index++;
         this.loop();
         break;
